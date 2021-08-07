283033cab5d1c193627127a817c6b5d641150f79
hjl <1196455147@qq.com>
2021-08-07 22:46:10 +0800

    :sparkles: add vecotr_free test

---
+++ C++/stl_container/vector_free.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+class Node
+{
+public:
+    int i = 0;
+    int *p = nullptr;
+    Node(int i) : i(i)
+    {
+        p = new int[i];
+        std::cout << "Node Constructor" << std::endl;
+    }
+    Node(const Node &other) : i(other.i + 1)
+    {
+        // memcpy(p, other.p, sizeof(int)*i);
+        std::cout << "Node Copy Constructor" << std::endl;
+    }
+    ~Node()
+    {
+        if (p != nullptr)
+        {
+            delete[] p;
+            p = nullptr;
+        }
+        std::cout << "Node Destructor" << std::endl;
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    {
+        Node n(1);
+    }
+
+    // void *;
+
+    std::vector<Node> nodelist;
+    nodelist.push_back(Node(1));
+    // std::cout << nodelist.size() << std::endl;
+}
--- C/gcc/builtin.c
+++ C/gcc/builtin.c
@@ -5,6 +5,24 @@
 #include <stdlib.h>
 #include <time.h>
 
+/*
+
+#!/bin/bash
+rm -f /data/pmem0/vecsearch1/*
+make clean
+make
+sudo perf record -g -F 10000 ./demo --nodes 100000 --test_count 200000 --populate true
+sudo perf record -g -F 10000 ./demo --nodes 100000 --test_count 200000 --populate false
+sudo perf report -n --stdio >call_function.txt
+
+sudo perf script -i perf.data &>perf.unfold
+
+/home/hjl/download/FlameGraph/stackcollapse-perf.pl perf.unfold &>perf.folded
+
+sudo /home/hjl/download/FlameGraph/flamegraph.pl perf.folded >perf.svg
+
+*/
+
 #define SIMD 1
 #define SIMD_CMP8(src, key)                                      \
     do                                                           \
@@ -43,7 +61,7 @@ static inline int find_last_set_64(uint64_t val)
 }
 
 // -------- 点乘 dot multly 加速--------------//
-#define N (64000000)
+#define N (6400000)
 #define ALIGNED_BYTES (8)
 /**
  * 
@@ -64,23 +82,39 @@ float dot_raw(const float *x, const float *y, int f)
 
 /**
  * 
- * dot _mm256_dp_ps  double版本
+ * dot _mm256_dp_ps  float版本
  * 
 **/
 
-float dot_mm256(float *a, float *b, int n)
+float dot_mm256(const float *x, const float *y, int len)
 {
-    double res = 0.0;
-    int k = n / 4 + 1;
-    for (int i = 0; i < k; i++)
-    {
-        __m256 ai = _mm256_loadu_ps(a + i * 4); //不对齐加载数据
-        __m256 bi = _mm256_loadu_ps(b + i * 4);
-        __m256 r = _mm256_dp_ps(ai, bi, 0xF1);
-        res += r[0] + r[5];
-        // res += r[5];
+
+    float inner_prod = 0.0f;
+    __m256 X, Y;                      //Declare two variables stored in the 128-bit special register of SSE
+    __m256 acc = _mm256_setzero_ps(); // Declare a variable stored in a 128-bit special register in SSE to store the result of X+Y, the initial value is 0
+    float temp[8];                    //Store the parameters of the intermediate variables
+
+    long i;
+    for (i = 0; i + 8 < len; i += 8)
+    {                                                  //128-bit special register, which can handle 4 groups of 32-bit variable operations at one time
+        X = _mm256_loadu_ps(x + i);                    // Load x to X (because 128 bits can store four 32-bit data, the default is to load 4 consecutive parameters at a time)
+        Y = _mm256_loadu_ps(y + i);                    //Same as above
+        acc = _mm256_add_ps(acc, _mm256_mul_ps(X, Y)); //x*y, the sum of x1*y1 in each round, the sum of x2*y2, the sum of x3*y3, the sum of x4*y4, and the final result The four sums are placed in the 128-bit register of acc.
     }
-    return res;
+    _mm256_storeu_ps(&temp[0], acc); // Load 4 32-bit data in acc into memory
+    //dot multiplication sum
+    long j;
+    for (j = 0; j < 8; j++)
+    {
+        inner_prod += temp[j];
+    }
+
+    // Just processed the dot multiplication of elements that are multiples of the first 4. If len is not a multiple of 4, then there is a little tail to deal with
+    for (; i < len; ++i)
+    {
+        inner_prod += x[i] * y[i]; //Continue to accumulate the product of the little tail
+    }
+    return inner_prod; //You're done
 }
 
 /**
@@ -113,8 +147,34 @@ float dot_mm128(const float *x, const float *y, const long len)
     }
     return inner_prod; //You're done
 }
+static inline float horizontal_add_v256(__m256 v)
+{
+    __m256 x1 = _mm256_hadd_ps(v, v);
+    __m256 x2 = _mm256_hadd_ps(x1, x1);
+    __m128 x3 = _mm256_extractf128_ps(x2, 1);
+    __m128 x4 = _mm_add_ss(_mm256_castps256_ps128(x2), x3);
 
-float dot_mm512(const float *x, const float *y, const long len)
+    return _mm_cvtss_f32(x4);
+}
+
+__attribute__((noinline)) float dot_mm256_horadd(const float *x, const float *y, const long len)
+{
+    __m256 ymm_sum0 = _mm256_setzero_ps();
+    __m256 ymm_sum1 = _mm256_setzero_ps();
+
+    for (size_t i = 0; i < len; i += 16)
+    {
+        ymm_sum0 = _mm256_fmadd_ps(_mm256_loadu_ps(x + i),
+                                   _mm256_loadu_ps(y + i),
+                                   ymm_sum0);
+        ymm_sum1 = _mm256_fmadd_ps(_mm256_loadu_ps(x + i + 8),
+                                   _mm256_loadu_ps(y + i + 8),
+                                   ymm_sum1);
+    }
+    return horizontal_add_v256(_mm256_add_ps(ymm_sum0, ymm_sum1));
+}
+
+__attribute__((noinline)) float dot_mm512(const float *x, const float *y, const long len)
 {
     float inner_prod = 0.0f;
     __m512 X, Y;                      //Declare two variables stored in the 128-bit special register of SSE
@@ -144,6 +204,32 @@ float dot_mm512(const float *x, const float *y, const long len)
     return inner_prod; //You're done
 }
 
+static inline float horizontal_add_v512(__m512 v)
+{
+    __m256 low = _mm512_castps512_ps256(v);
+    __m256 high = _mm256_castpd_ps(_mm512_extractf64x4_pd(
+        _mm512_castps_pd(v), 1));
+
+    return horizontal_add_v256(low + high);
+}
+
+__attribute__((noinline)) float dot_fma512_horadd(const float *x, const float *y, const long len)
+{
+    __m512 zmm_sum0 = _mm512_setzero_ps();
+    __m512 zmm_sum1 = _mm512_setzero_ps();
+
+    for (size_t i = 0; i < len; i += 32)
+    {
+        zmm_sum0 = _mm512_fmadd_ps(_mm512_loadu_ps(x + i),
+                                   _mm512_loadu_ps(y + i),
+                                   zmm_sum0);
+        zmm_sum1 = _mm512_fmadd_ps(_mm512_loadu_ps(x + i + 16),
+                                   _mm512_loadu_ps(y + i + 16),
+                                   zmm_sum1);
+    }
+    return horizontal_add_v512(_mm512_add_ps(zmm_sum0, zmm_sum1));
+}
+
 __attribute__((noinline)) float dot512fma2(float *x1, float *x2, size_t len)
 {
     // assert(len % 32 == 0);
@@ -164,6 +250,41 @@ __attribute__((noinline)) float dot512fma2(float *x1, float *x2, size_t len)
             __m512 v22 = _mm512_loadu_ps(x2 + i + 16);
             sum = _mm512_fmadd_ps(v11, v21, sum);
             sum = _mm512_fmadd_ps(v12, v22, sum);
+        }
+    }
+    float buffer[16];
+    _mm512_storeu_ps(buffer, sum);
+    return buffer[0] + buffer[1] + buffer[2] + buffer[3] + buffer[4] + buffer[5] +
+           buffer[6] + buffer[7] + buffer[8] + buffer[9] + buffer[10] +
+           buffer[11] + buffer[12] + buffer[13] + buffer[14] + buffer[15];
+}
+
+__attribute__((noinline)) float dot512fma4(float *x1, float *x2, size_t len)
+{
+    // assert(len % 32 == 0);
+    if (len % 64 != 0)
+    {
+        return 0;
+    }
+    __m512 sum = _mm512_setzero_ps();
+    if (len > 63)
+    {
+        size_t limit = len - 63;
+        for (size_t i = 0; i < limit; i += 64)
+        {
+
+            __m512 v11 = _mm512_loadu_ps(x1 + i);
+            __m512 v21 = _mm512_loadu_ps(x2 + i);
+            __m512 v12 = _mm512_loadu_ps(x1 + i + 16);
+            __m512 v22 = _mm512_loadu_ps(x2 + i + 16);
+            __m512 v13 = _mm512_loadu_ps(x1 + i + 32);
+            __m512 v23 = _mm512_loadu_ps(x2 + i + 32);
+            __m512 v14 = _mm512_loadu_ps(x1 + i + 48);
+            __m512 v24 = _mm512_loadu_ps(x2 + i + 48);
+            sum = _mm512_fmadd_ps(v11, v21, sum);
+            sum = _mm512_fmadd_ps(v12, v22, sum);
+            sum = _mm512_fmadd_ps(v13, v23, sum);
+            sum = _mm512_fmadd_ps(v14, v24, sum);
         }
     }
     float buffer[16];
@@ -249,6 +370,12 @@ int main()
     printf("pall times = %fs\n", (double)(end_time - start_time) / CLOCKS_PER_SEC);
 
     start_time = clock();
+    r1 = dot_mm256_horadd(d1, d2, N);
+    end_time = clock();
+    printf("MM256_horadd dot:%f\n", r1);
+    printf("pall times = %fs\n", (double)(end_time - start_time) / CLOCKS_PER_SEC);
+
+    start_time = clock();
     r1 = dot_mm512(d1, d2, N);
     end_time = clock();
     printf("MM512 dot:%f\n", r1);
@@ -260,6 +387,18 @@ int main()
     printf("MM512fma2 dot:%f\n", r1);
     printf("pall times = %fs\n", (double)(end_time - start_time) / CLOCKS_PER_SEC);
 
+    start_time = clock();
+    r1 = dot512fma4(d1, d2, N);
+    end_time = clock();
+    printf("MM512fma4 dot:%f\n", r1);
+    printf("pall times = %fs\n", (double)(end_time - start_time) / CLOCKS_PER_SEC);
+
+    start_time = clock();
+    r1 = dot_fma512_horadd(d1, d2, N);
+    end_time = clock();
+    printf("MM512_fma512_horadd dot:%f\n", r1);
+    printf("pall times = %fs\n", (double)(end_time - start_time) / CLOCKS_PER_SEC);
+
     r1 = dot_raw(d3, d4, N);
     start_time = clock();
     r1 = dot512fma2_align(d3, d4, N);
@@ -269,4 +408,6 @@ int main()
 
     free(d1);
     free(d2);
+    free(d3);
+    free(d4);
 }
