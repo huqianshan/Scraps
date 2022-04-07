9b4dbef41c45364e12ab80bcec8e1a63d7056819
hjl-N501 <1196455147@qq.com>
2022-04-07 15:44:24 +0800

    :construction: add bash and init.sh

--- Config/init.sh
+++ Config/init.sh
@@ -9,4 +9,4 @@ sudo apt-get install git make cmake  ssh ssh-server
 sudo apt-get install zsh autojump
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
